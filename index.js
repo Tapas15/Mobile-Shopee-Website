4b20ec418c901b917b744ddae92bc73ab020ecdf
Tapas <tapas.mahanandia@gmail.com>
2021-01-01 13:43:14 +0530

    top sale commited

--- index.js
+++ index.js
@@ -4,5 +4,22 @@ $(document).ready(function(){
         dots: true,
         items: 1
     });
+    // Top sale 
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
   
     });
