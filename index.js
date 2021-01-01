4be3d5a6faaf317489837898cb61ddf514d4b9d3
Tapas <tapas.mahanandia@gmail.com>
2021-01-01 13:05:14 +0530

    not succeffull

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
     // banner owl carousel
-    $("#banner-area .owl-carousel").owlCarousel({
-        dots: true,
+    $("#banner-area.owl-carousel").owlCarousel({
+        dots: true,  
         items: 1
     });
-}
+});
